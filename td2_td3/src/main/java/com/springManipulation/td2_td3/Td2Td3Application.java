45be6500be6fb8354dac718328f361ef5bbc8e70
kenny0023 <hei.bryan.2@gmail.com>
2026-03-19 09:07:34 +0100

    création de dossier

---
+++ td2_td3/src/main/java/com/springManipulation/td2_td3/Td2Td3Application.java
@@ -0,0 +1,13 @@
+package com.springManipulation.td2_td3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2Td3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td2Td3Application.class, args);
+	}
+
+}
